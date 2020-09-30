d7dd7097887a606e378bd40bdaeaa6dfd1836e56
suho <suho950509@gmail.com>
2020-09-30 15:39:18 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 6000
+const bodyParser = require('body-parser'); //bodyParser 을 이용하기위해 가져오자.
+const { User } = require("./models/User"); //post를 이용한 데이터데이스를 가져올 떄 아까 만들어 놓은 스키마를 이용할 필요가 있다.
+const config = require("./config/key");
 
 
-const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://suho:sasugasuho1!@suho.3663u.mongodb.net/<dbname>?retryWrites=true&w=majority
-`, {useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false
+//body-parser에 옵션 주기
+// 1. application/x-www-form-urlencoded 형태의 데이터 를 가져와서 분석할 수 있게끔
+app.use(bodyParser.urlencoded({extended: true}));
+// 2. application/json 형태의 데이터를 가져와서 분석할 수 있게끔.
+app.use(bodyParser.json());
+
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI,
+ {useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false
 }).then(() => console.log(`MongoDB Connected...`))
 .catch(err => console.log(err));
 
@@ -14,6 +24,22 @@ mongoose.connect(`mongodb+srv://suho:sasugasuho1!@suho.3663u.mongodb.net/<dbname
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//회원가입을 위한 라우터 만들기
+app.post('/register', (req,res) => {
+//회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+    //bodyParser 가 보내주는 역할 하는것임.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({ success: true
+        })
+    })
+})
+
+app.get('/', (req,res) => res.send('Hello. nodeJS'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
