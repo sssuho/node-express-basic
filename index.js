2e4bde9825c566b2c612d72b67882426620369c3
suho <suho950509@gmail.com>
2020-10-04 15:31:16 +0900

    로그인 부분 완성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 6000
 const bodyParser = require('body-parser'); //bodyParser 을 이용하기위해 가져오자.
+const cookieParser = require('cookie-parser');
 const { User } = require("./models/User"); //post를 이용한 데이터데이스를 가져올 떄 아까 만들어 놓은 스키마를 이용할 필요가 있다.
 const config = require("./config/key");
 
@@ -11,6 +12,7 @@ const config = require("./config/key");
 app.use(bodyParser.urlencoded({extended: true}));
 // 2. application/json 형태의 데이터를 가져와서 분석할 수 있게끔.
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose');
@@ -40,6 +42,45 @@ app.post('/register', (req,res) => {
 })
 
 app.get('/', (req,res) => res.send('Hello. nodeJS'));
+
+app.post('/login', (req, res) => {
+
+  // console.log('ping')
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+
+    // console.log('user', user)
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // console.log('err',err)
+
+      // console.log('isMatch',isMatch)
+
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 (여기서는 쿠키에 저장
+        // 쿠키에 저장하기 위해서는 라이브러리를 깔아야함. express에서 제공하는 cookie-parser 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
